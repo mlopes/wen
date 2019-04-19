8743e1546779e251dd9fadd222f3368e9f9b6e78
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-04-19 17:52:44 +0100

    Test Year with scalacheck

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
   lazy val scalaTest = Seq(
     "org.scalatest" %% "scalatest" % "3.0.5" % Test
     ,"org.scalactic" %% "scalactic" % "3.0.5" % Test
+    , "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
   )
 
   lazy val wenDependencies = Seq(
--- src/test/scala/com/mlopes/wen/types/YearSpec.scala
+++ src/test/scala/com/mlopes/wen/types/YearSpec.scala
@@ -1,33 +1,66 @@
 package com.mlopes.wen.types
 
+import org.scalacheck.Gen
+import org.scalacheck.Prop.forAll
 import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.prop.Checkers
 import org.scalatest.{Matchers, WordSpec}
 
-class YearSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
+class YearSpec extends WordSpec with Matchers with TypeCheckedTripleEquals with Checkers {
 
   "Year" should {
 
     "be created with a positive number" in {
-      val year = Year(2019, BC)
-      year.get.year.value should ===(2019)
-      year.get.epoch should ===(BC)
+      val year = for {
+        y <- Gen.posNum[Int]
+        e <- Gen.oneOf(BC, AC)
+      } yield Year(y, e)
+
+      val prop = forAll(year) { y =>
+        y !==(None)
+      }
+      check(prop)
     }
 
     "fail to be created with a negative number" in {
-      Year(-2019, AC) should ===(None)
+      val year = for {
+        y <- Gen.negNum[Int]
+        e <- Gen.oneOf(BC, AC)
+      } yield Year(y, e)
+
+      val prop = forAll(year) { y =>
+        y ===(None)
+      }
+      check(prop)
     }
 
     "fail to be created with a zero" in {
-      Year(0, AC) should ===(None)
+      val year = for {
+        e <- Gen.oneOf(BC, AC)
+      } yield Year(0, e)
+
+      val prop = forAll(year) { y =>
+        y ===(None)
+      }
+      check(prop)
     }
 
     "be pattern matched" in {
-      Year(2019, AC).get match {
-        case Year(y, e) =>
-          y.value should ===(2019)
-          e should ===(AC)
-        case _ => fail()
+      val year = for {
+        y <- Gen.posNum[Int]
+        e <- Gen.oneOf(BC, AC)
+      } yield Year(y, e)
+
+      val prop = forAll(year) { y =>
+
+        val matchResult = y.get match {
+          case Year(y1, e) =>
+            y.get.year ==(y1) && y.get.epoch ===(e)
+          case _ => false
+        }
+        matchResult == true
       }
+      check(prop)
     }
   }
 }
