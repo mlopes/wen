a28cf1f8dbf8247ce45995093b17ea9f9b01e561
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-04-20 08:36:27 +0100

    Add Show instance for Month

--- src/test/scala/com/mlopes/wen/instances/MonthInstancesSpec.scala
+++ src/test/scala/com/mlopes/wen/instances/MonthInstancesSpec.scala
@@ -2,8 +2,8 @@ package com.mlopes.wen.instances
 
 import cats._
 import org.scalatest.{Matchers, WordSpec}
-import com.mlopes.wen.types.{August, December, February, January, July, March, May, Month, September}
-import com.mlopes.wen.instances.MonthInstances.monthOrderInstance
+import com.mlopes.wen.types._
+import com.mlopes.wen.instances.MonthInstances._
 import org.scalactic.TypeCheckedTripleEquals
 
 class MonthInstancesSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
@@ -17,6 +17,21 @@ class MonthInstancesSpec extends WordSpec with Matchers with TypeCheckedTripleEq
     "provide eq" in {
       Eq[Month].eqv(December, December) should ===(true)
       Eq[Month].eqv(September, March) should ===(false)
+    }
+
+    "provide show" in {
+      Show[Month].show(January) should ===("January")
+      Show[Month].show(February) should ===("February")
+      Show[Month].show(March) should ===("March")
+      Show[Month].show(April) should ===("April")
+      Show[Month].show(May) should ===("May")
+      Show[Month].show(June) should ===("June")
+      Show[Month].show(July) should ===("July")
+      Show[Month].show(August) should ===("August")
+      Show[Month].show(September) should ===("September")
+      Show[Month].show(October) should ===("October")
+      Show[Month].show(November) should ===("November")
+      Show[Month].show(December) should ===("December")
     }
   }
 }
