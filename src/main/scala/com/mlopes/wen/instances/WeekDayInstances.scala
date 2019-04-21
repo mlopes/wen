1b6e82843ac26e9763bdb7e1aefead8b29c86c46
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-04-21 22:01:57 +0100

    Add instances for WeekDay

---
+++ src/main/scala/com/mlopes/wen/instances/WeekDayInstances.scala
@@ -0,0 +1,35 @@
+package com.mlopes.wen.instances
+
+import cats.implicits._
+import cats.{Order, Show}
+import com.mlopes.wen.types._
+
+object WeekDayInstances {
+
+  implicit val weekDayOrderInstance: Order[WeekDay] = new Order[WeekDay] {
+    override def compare(x: WeekDay, y: WeekDay): Int = {
+      lazy val weekDayToInt: WeekDay => Int = {
+        case Sunday => 1
+        case Monday => 2
+        case Tuesday => 3
+        case Wednesday => 4
+        case Thursday => 5
+        case Friday => 6
+        case Saturday => 7
+      }
+      Order[Int].compare(weekDayToInt(x), weekDayToInt(y))
+    }
+  }
+
+  implicit val weekdayShowInstance: Show[WeekDay] = new Show[WeekDay] {
+    override def show(t: WeekDay): String = t match {
+      case Sunday => "Sunday"
+      case Monday => "Monday"
+      case Tuesday => "Tuesday"
+      case Wednesday => "Wednesday"
+      case Thursday => "Thursday"
+      case Friday => "Friday"
+      case Saturday => "Saturday"
+    }
+  }
+}
---
+++ src/test/scala/com/mlopes/wen/instances/WeekDayInstancesSpec.scala
@@ -0,0 +1,35 @@
+package com.mlopes.wen.instances
+
+import cats._
+import com.mlopes.wen.instances.WeekDayInstances._
+import com.mlopes.wen.types._
+import org.scalactic.TypeCheckedTripleEquals
+import org.scalatest.{Matchers, WordSpec}
+
+class WeekDayInstancesSpec extends WordSpec with Matchers with TypeCheckedTripleEquals {
+
+  "WeekDay Instances" should {
+
+    "provide order" in {
+      (Order[WeekDay].compare(Sunday, Monday) < 0) should  ===(true)
+      (Order[WeekDay].compare(Wednesday, Monday) > 0) should  ===(true)
+      (Order[WeekDay].compare(Tuesday, Tuesday) ===(0)) should  ===(true)
+    }
+
+    "provide eq" in {
+      Eq[WeekDay].eqv(Friday, Friday) should ===(true)
+      Eq[WeekDay].eqv(Saturday, Friday) should ===(false)
+    }
+
+    "provide show" in {
+      Show[WeekDay].show(Sunday) should ===("Sunday")
+      Show[WeekDay].show(Monday) should ===("Monday")
+      Show[WeekDay].show(Tuesday) should ===("Tuesday")
+      Show[WeekDay].show(Wednesday) should ===("Wednesday")
+      Show[WeekDay].show(Thursday) should ===("Thursday")
+      Show[WeekDay].show(Friday) should ===("Friday")
+      Show[WeekDay].show(Saturday) should ===("Saturday")
+    }
+
+  }
+}
