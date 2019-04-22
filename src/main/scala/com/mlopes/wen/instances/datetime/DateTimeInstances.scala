9b15c086825aa4f501e6f870994f0f9d3b14673c
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-04-22 21:28:45 +0100

    Add instances for Zone Date Time

--- src/main/scala/com/mlopes/wen/instances/datetime/DateTimeInstances.scala
+++ src/main/scala/com/mlopes/wen/instances/datetime/DateTimeInstances.scala
@@ -2,23 +2,35 @@ package com.mlopes.wen.instances.datetime
 
 import cats.{Order, Show}
 import cats.implicits._
-import com.mlopes.wen.datetime.DateTime
+import com.mlopes.wen.datetime.{DateTime, ZoneDateTime}
 import com.mlopes.wen.instances.datetime.DateInstances._
 import com.mlopes.wen.instances.datetime.TimeInstances._
 
 object DateTimeInstances {
 
   implicit var dateTimeOrderInstance: Order[DateTime] = new Order[DateTime] {
-    override def compare(x: DateTime, y: DateTime): Int = {
+    override def compare(x: DateTime, y: DateTime): Int =
       if (x.date === y.date) {
         x.time compare y.time
       } else {
         x.date compare y.date
       }
-    }
+  }
+
+  implicit var zoneDateTimeOrderInstance: Order[ZoneDateTime] = new Order[ZoneDateTime] {
+    override def compare(x: ZoneDateTime, y: ZoneDateTime): Int =
+      if(x.date === y.date) {
+        x.zoneTime compare y.zoneTime
+      } else {
+        x.date compare y.date
+      }
   }
 
   implicit var dateTimeShowInstance: Show[DateTime] = new Show[DateTime] {
     override def show(t: DateTime): String = s"${t.date.show} ${t.time.show}"
+  }
+
+  implicit var zoneDateTimeShowInstance: Show[ZoneDateTime] = new Show[ZoneDateTime] {
+    override def show(t: ZoneDateTime): String = s"${t.date.show} ${t.zoneTime.show}"
   }
 }
