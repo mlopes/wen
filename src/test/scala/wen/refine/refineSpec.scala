bb40bad730c93f6dbc6b51a0edc02005a9d26e51
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-05-02 16:19:32 +0100

    Fix missing property checks on refineSpec

--- src/test/scala/wen/refine/refineSpec.scala
+++ src/test/scala/wen/refine/refineSpec.scala
@@ -3,170 +3,218 @@ package wen.refine
 import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.WordSpec
 import org.scalacheck.Prop.forAll
+import org.scalatestplus.scalacheck.Checkers
 
-class refineSpec extends WordSpec {
+class refineSpec extends WordSpec with Checkers {
 
-  "refineSpec" should {
+  "refineSecond" should {
 
-    "refine Second should succeed" in {
+    "refine Second" in {
       val second = Gen.choose(0, 59)
 
-      forAll(second) { s =>
+      val prop = forAll(second) { s: Int =>
         refineSecond(s) match {
-          case Right(s1) => s1 ===(s)
+          case Right(s1) => s1.value === (s)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Second should fail" in {
+    "fail to refine non-second" in {
       val notASecond = Arbitrary.arbitrary[Int] suchThat (x => x < 0 || x > 59)
 
-      forAll(notASecond) { s =>
+      val prop = forAll(notASecond) { s =>
         refineSecond(s) match {
           case Left(_) => true
           case _ => false
         }
       }
-    }
 
-    "refine Minute should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineMinute" should {
+
+    "refine Minute" in {
       val minute = Gen.choose(0, 59)
 
-      forAll(minute) { m =>
+      val prop = forAll(minute) { m =>
         refineMinute(m) match {
-          case Right(m1) => m1 ===(m)
+          case Right(m1) => m1.value === (m)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Minute should fail" in {
+    "fail to refine non-minute" in {
       val notAMinute = Arbitrary.arbitrary[Int] suchThat (x => x < 0 || x > 59)
 
-      forAll(notAMinute) { m =>
+      val prop = forAll(notAMinute) { m =>
         refineMinute(m) match {
           case Left(_) => true
           case _ => false
         }
       }
-    }
 
-    "refine Year should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineYear" should {
+
+    "refine Year" in {
       val year = Gen.posNum[Int]
 
-      forAll(year) { y =>
+      val prop = forAll(year) { y =>
         refineYear(y) match {
-          case Right(y1) => y1 ===(y)
+          case Right(y1) => y1.value === (y)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Year should fail for negative numbers" in {
+    "fail to refine negative numbers" in {
       val notAYear = Gen.negNum[Int]
 
-      forAll(notAYear) { y =>
+      val prop = forAll(notAYear) { y =>
         refineYear(y) match {
           case Left(_) => true
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Year should fail for 0" in {
-      refineYear(0) match {
+    "should fail to refine 0" in {
+      val prop = refineYear(0) match {
         case Left(_) => true
         case _ => false
       }
-    }
 
-    "refine Hour should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineHour" should {
+
+    "refine Hour" in {
       val hour = Gen.choose(1, 23)
 
-      forAll(hour) { h =>
+      val prop = forAll(hour) { h =>
         refineHour(h) match {
-          case Right(h1) => h1 ===(h)
+          case Right(h1) => h1.value === (h)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Hour should fail" in {
+    "fail to refine non-hour" in {
       val notAHour = Arbitrary.arbitrary[Int] suchThat (x => x < 0 || x > 23)
 
-      forAll(notAHour) { h =>
+      val prop = forAll(notAHour) { h =>
         refineHour(h) match {
           case Left(_) => true
           case _ => false
         }
       }
-    }
 
-    "refine Millisecond should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineMillisecond" should {
+
+    "refine Millisecond" in {
       val millisecond = Gen.choose(0, 999)
 
-      forAll(millisecond) { m =>
+      val prop = forAll(millisecond) { m =>
         refineMilliSecond(m) match {
-          case Right(m1) => m1 ===(m)
+          case Right(m1) => m1.value === (m)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Millisecond should fail" in {
+    "fail to refine non-millisecond" in {
       val notAMillisecond = Arbitrary.arbitrary[Int] suchThat (x => x < 0 || x > 999)
 
-      forAll(notAMillisecond) { m =>
+      val prop = forAll(notAMillisecond) { m =>
         refineMilliSecond(m) match {
           case Left(_) => true
           case _ => false
         }
       }
-    }
 
-    "refine Day should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineDay" should {
+
+    "refine Day" in {
       val day = Gen.choose(1, 31)
 
-      forAll(day) { d =>
+      val prop = forAll(day) { d =>
         refineDay(d) match {
-          case Right(d1) => d1 ===(d)
+          case Right(d1) => d1.value === (d)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Day should fail" in {
+    "fail to refine non-day" in {
       val notADay = Arbitrary.arbitrary[Int] suchThat (x => x < 1 || x > 31)
 
-      forAll(notADay) { d =>
+      val prop = forAll(notADay) { d =>
         refineMonth(d) match {
           case Left(_) => true
           case _ => false
         }
       }
-    }
 
-    "refine Month should succeed" in {
+      check(prop)
+    }
+  }
+
+  "refineMonth" should {
+    "refine Month" in {
       val month = Gen.choose(1, 12)
 
-      forAll(month) { m =>
+      val prop = forAll(month) { m =>
         refineMonth(m) match {
-          case Right(m1) => m1 ===(m)
+          case Right(m1) => m1.value ===(m)
           case _ => false
         }
       }
+
+      check(prop)
     }
 
-    "refine Month should fail" in {
+    "fail to refine non-month" in {
       val notAMonth = Arbitrary.arbitrary[Int] suchThat (x => x < 1 || x > 12)
 
-      forAll(notAMonth) { m =>
+      val prop = forAll(notAMonth) { m =>
         refineMonth(m) match {
           case Left(_) => true
           case _ => false
         }
       }
+
+      check(prop)
     }
   }
 }
